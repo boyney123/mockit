923b889b248e394246de18bcfdd339d4cb18c143
David Boyne <davidboyne123@hotmail.co.uk>
2019-04-17 17:13:54 +0100

    Setting up codecov

--- install-and-test.sh
+++ install-and-test.sh
@@ -1,5 +1,5 @@
 dir=$(pwd)
 CI=true
 cd $dir/mockit-routes && npm install && npm test .
-cd $dir/client && npm install && CI=true npm test
+cd $dir/client && npm install && CI=true npm test -- --coverage
 cd $dir/server && npm install && npm test
