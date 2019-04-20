052aad4bd06be5b7c71d62d40ff22b1bb424f695
Hugo Locurcio <hugo.locurcio@hugo.pro>
2019-04-20 22:57:50 +0200

    Fix shellcheck warnings in shell scripts
    This should make them work if the PWD contains spaces.

--- build-and-start.sh
+++ build-and-start.sh
@@ -1,5 +1,7 @@
+#!/bin/sh
+
 dir=$(pwd)
-cd $dir/mockit-routes && docker build -t mockit-routes .
-cd $dir/client && docker build -t mockit-client .
-cd $dir/server && docker build -t mockit-server .
+cd "$dir/mockit-routes" && docker build -t mockit-routes .
+cd "$dir/client" && docker build -t mockit-client .
+cd "$dir/server" && docker build -t mockit-server .
 docker-compose up -d
