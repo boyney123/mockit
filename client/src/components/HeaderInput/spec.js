6451410b686b208f9acb83265e5ad129e56d94d4
David Boyne <davidboyne123@hotmail.co.uk>
2019-05-24 20:48:07 +0100

    1.1.0 (#48)
    * Add the ability to group routes (#42)
    
    * First version of grouping routes
    
    * Switched to new calculation of groups and styled them
    
    * Added group routing based on togglable state
    
    * Updated package-lock
    
    * DB - Moved some components around, added some tests and fixed jest running tests
    
    * DB - Added more test coverage
    
    * Removed useless header x-powered-by (#45)
    
    * Improved methods and status codes (#44)
    
    * DB - Fixing tests for new routes that were added as part of #44
    
    * Added CHANGELOG
    
    * Feature - Add support for adding headers onto responses (#46)
    
    * Started to add custom headers
    
    * Tmp
    
    * #4 - Added the ability to add headers in the routes
    
    * Updated CHANGELOG
    
    * DB - Format of settings
    
    * Updated version

---
+++ client/src/components/HeaderInput/spec.js
@@ -0,0 +1,55 @@
+// __tests__/fetch.test.js
+import React from "react";
+import { render, fireEvent, cleanup } from "react-testing-library";
+import "jest-dom/extend-expect";
+import HeaderInput from "./";
+
+afterEach(cleanup);
+
+describe("DoubleInput", () => {
+  describe("renders", () => {
+    it("two inputs one for the header and one for the value", () => {
+      const { getByPlaceholderText } = render(<HeaderInput />);
+      expect(getByPlaceholderText("header")).toBeVisible();
+      expect(getByPlaceholderText("value")).toBeVisible();
+    });
+
+    it('two inputs are rendered with the given "header" and "value" data when given to the component', () => {
+      const { getByPlaceholderText } = render(<HeaderInput data={{ id: 1, header: "Content-Type", value: "application/json" }} />);
+      expect(getByPlaceholderText("header").value).toEqual("Content-Type");
+      expect(getByPlaceholderText("value").value).toEqual("application/json");
+    });
+  });
+
+  describe("props: events", () => {
+    it('onBlur is called when both "header" and "value" have been entered', () => {
+      const spy = jest.fn();
+      const { getByPlaceholderText } = render(<HeaderInput onBlur={spy} />);
+      fireEvent.change(getByPlaceholderText("header"), { target: { value: "Content-Type" } });
+      fireEvent.change(getByPlaceholderText("value"), { target: { value: "application/json" } });
+      expect(spy).toHaveBeenCalled();
+    });
+
+    it('onBlur is not called when "header" value is set but "value" is missing', () => {
+      const spy = jest.fn();
+      const { getByPlaceholderText } = render(<HeaderInput onBlur={spy} />);
+      fireEvent.change(getByPlaceholderText("header"), { target: { value: "Content-Type" } });
+      expect(spy).not.toHaveBeenCalled();
+    });
+
+    it('onBlur is not called when "value" is set but the "header" value is not', () => {
+      const spy = jest.fn();
+      const { getByPlaceholderText } = render(<HeaderInput onBlur={spy} />);
+      fireEvent.change(getByPlaceholderText("value"), { target: { value: "application/json" } });
+      expect(spy).not.toHaveBeenCalled();
+    });
+
+    it("onRemove is called with the headers id when the user clicks on the remove icon", () => {
+      const spy = jest.fn();
+      const data = { id: 1, header: "Content-Type", value: "application/json" };
+      const { getByLabelText } = render(<HeaderInput data={data} onRemove={spy} />);
+      fireEvent.click(getByLabelText("remove-header"));
+      expect(spy).toHaveBeenCalledWith(1);
+    });
+  });
+});
